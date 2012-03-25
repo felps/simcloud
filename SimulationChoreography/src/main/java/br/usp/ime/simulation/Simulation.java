18154af17dc13f812d120f9d8f84ccd769bb1aaf
Eduardo Kuroda <eduardohideo@gmail.com>
2012-03-25 00:07:45 -0300

    refatored Simulation

--- SimulationChoreography/src/main/java/br/usp/ime/simulation/Simulation.java
+++ SimulationChoreography/src/main/java/br/usp/ime/simulation/Simulation.java
@@ -16,25 +16,26 @@ public class Simulation {
     * which also contains such a launcher
     */
    
-    public static void main(String[] args) throws NativeException {
+    public static void main(final String[] args) throws NativeException {
     String platform = "smallplatform.xml";
     String deploymentFile = "smallOrchestrationDeployment.xml";
     
 	/* initialize the MSG simulation. Must be done before anything else (even logging). */
 	Msg.init(args);
 
-    if(args.length != 2) {    		
-	  Msg.info("Usage   : Simulation platform_file deployment_file");
-	  Msg.info("example : Simulation comm_time_platform.xml comm_time_deployment.xml");
-    } else if(args.length == 2){
-    		platform = args[0];
-    		deploymentFile = args[1];
-    		}
+    if(args.length == 2) {
+    	platform = args[0];
+    	deploymentFile = args[1];
+    	}
+    else{
+    	Msg.info("Usage   : Simulation platform_file deployment_file");
+        Msg.info("example : Simulation comm_time_platform.xml comm_time_deployment.xml");
+    }
 	/* construct the platform and deploy the application */
 	Msg.createEnvironment(platform);
 	Msg.deployApplication(deploymentFile);
 		
 	/*  execute the simulation. */
-        Msg.run();
+	Msg.run();
     }
 }
